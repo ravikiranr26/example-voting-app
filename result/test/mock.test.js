ad29f4b06eee05fd68b2cff4aa386aa931256b4e
ravikiranr26 <rknmit@gmail.com>
2022-01-23 23:49:43 +0100

    adding docker packaging for all apps

--- result/test/mock.test.js
+++ result/test/mock.test.js
@@ -20,8 +20,8 @@ describe('mock test 3', () => {
 });
 
 
-describe('mock test 4', () => {
-  it('unit test 4', () => {
+describe('mock test 5', () => {
+  it('unit test 5', () => {
     expect(true).to.be.true;
   });
 });
